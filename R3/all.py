9f48737e24786722128541dd6099a74f1b497280
Ruben <Rthem@gmx.de>
2021-12-01 22:38:54 +0100

    R3_datas_lamp 14

--- R3/all.py
+++ R3/all.py
@@ -14,11 +14,12 @@ import RPi.GPIO as GPIO
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()  # uses board.SCL and board.SDA
 sensor = HTU21D(i2c)
-print("\nTemperature: %0.1f C" % sensor.temperature)
-print("Humidity: %0.1f %%" % sensor.relative_humidity)
 
-print(sensor.temperature)
-print(sensor.relative_humidity)
+# print("\nTemperature: %0.1f C" % sensor.temperature)
+# print("Humidity: %0.1f %%" % sensor.relative_humidity)
+
+# print(sensor.temperature)
+# print(sensor.relative_humidity)
 
 # temperatur, druck, x = bme280.readBME280All()
 # print("\nTemperatur : ", temperatur, "C")
@@ -26,9 +27,14 @@ print(sensor.relative_humidity)
 
 GPIO.setmode(GPIO.BCM)
 fields = [17, 27, 22, 23, 8, 7, 25, 16, 20, 21]
+tf = [17, 27, 22, 23, 8]
+hf = [7, 25, 16, 20, 21]
+e = 20
+k = 20
 
-for y in fields:
-    GPIO.setup(y, GPIO.OUT)
+while 1:
+    for y in fields:
+        GPIO.setup(y, GPIO.OUT)
 
 # GPIO.setup(17, GPIO.OUT)
 # GPIO.setup(27, GPIO.OUT)
@@ -41,27 +47,38 @@ for y in fields:
 # GPIO.setup(20, GPIO.OUT)
 # GPIO.setup(21, GPIO.OUT)
 
-# def
-if sensor.temperature > 23.5:
-    GPIO.output(17, 1)
+    for a in tf:
+        e = e + 2
+        if sensor.temperature > e:
+            GPIO.output(a, 1)
+        else:
+            GPIO.output(a, 0)
 
-if sensor.temperature > 24.5:
-    GPIO.output(27, 1)
+    for z in hf:
+        k = k + 2
+        if sensor.relative_humidity > k:
+            GPIO.output(z, 1)
+        else:
+            GPIO.output(z, 0)
 
-if sensor.temperature > 25.5:
-    GPIO.output(22, 1)
+#    if sensor.temperature > 23.5:
+#        GPIO.output(17, 1)
+#
+#    if sensor.temperature > 24.5:
+#        GPIO.output(27, 1)
+#
+#    if sensor.temperature > 25.5:
+#        GPIO.output(22, 1)
+#
+#    if sensor.temperature > 26.5:
+#        GPIO.output(23, 1)
+#
+#    if sensor.temperature > 27.5:
+#        GPIO.output(8, 1)
 
-if sensor.temperature > 26.5:
-    GPIO.output(23, 1)
-
-if sensor.temperature > 27.5:
-    GPIO.output(8, 1)
-
-time.sleep(2)
+    time.sleep(3)
 
 # for x in fields:
 #     GPIO.output(x, 1)
 #     time.sleep(0.2)
 #     GPIO.output(x, 0)
-
-GPIO.cleanup()
