05bbaac4a07996617f3c26bd9a5e99c58fc41d6c
Ruben <Rthem@gmx.de>
2021-12-01 22:22:59 +0100

    R3_datas_lamp 11

--- R3/all.py
+++ R3/all.py
@@ -27,16 +27,19 @@ print(sensor.relative_humidity)
 GPIO.setmode(GPIO.BCM)
 fields = [17, 27, 22, 23, 8, 7, 25, 16, 20, 21]
 
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(27, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(25, GPIO.OUT)
-GPIO.setup(8, GPIO.OUT)
-GPIO.setup(7, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT)
-GPIO.setup(21, GPIO.OUT)
+for y in fields:
+    GPIO.setup(y, GPIO.OUT)
+
+# GPIO.setup(17, GPIO.OUT)
+# GPIO.setup(27, GPIO.OUT)
+# GPIO.setup(22, GPIO.OUT)
+# GPIO.setup(23, GPIO.OUT)
+# GPIO.setup(25, GPIO.OUT)
+# GPIO.setup(8, GPIO.OUT)
+# GPIO.setup(7, GPIO.OUT)
+# GPIO.setup(16, GPIO.OUT)
+# GPIO.setup(20, GPIO.OUT)
+# GPIO.setup(21, GPIO.OUT)
 
 # def
 if sensor.temperature > 23:
@@ -58,7 +61,7 @@ time.sleep(2)
 
 for x in fields:
     GPIO.output(x, 1)
-    time.sleep(0.1)
+    time.sleep(0.2)
     GPIO.output(x, 0)
 
 GPIO.cleanup()
