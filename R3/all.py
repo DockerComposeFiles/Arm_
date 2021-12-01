f19c2c2fc1d9c735ffec430eca39bb498dcf9327
Ruben <Rthem@gmx.de>
2021-12-01 21:58:56 +0100

    R3_datas_lamp 3

--- R3/all.py
+++ R3/all.py
@@ -2,14 +2,12 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
+# HTU
 import time
+import board
 # BMP
 import bme280
-# HTU
-import board
 from adafruit_htu21d import HTU21D
-# Lampen
-import RPi.GPIO as GPIO
 
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -17,27 +15,9 @@ sensor = HTU21D(i2c)
 print("\nTemperature: %0.1f C" % sensor.temperature)
 print("Humidity: %0.1f %%" % sensor.relative_humidity)
 
+# Messdaten Holen BMP
 temperatur, druck, x = bme280.readBME280All()
 print("Temperatur : ", temperatur, "C")
 print("Druck: ", druck, "hPa")
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(27, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
-GPIO.setup(25, GPIO.OUT)
-GPIO.setup(8, GPIO.OUT)
-GPIO.setup(7, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT)
-GPIO.setup(21, GPIO.OUT)
-
-fields = [17, 27, 22, 23, 8, 7, 25, 16, 20, 21]
-for x in fields:
-    GPIO.output(x, 1)
-    time.sleep(0.5)
-    GPIO.output(x, 0)
-GPIO.cleanup()
-
+time.sleep(2)
 
